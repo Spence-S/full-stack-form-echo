08918470dc249bb3abc8747b4c9ee4ea9cd673ac
Spencer Snyder <sasnyde2@gmail.com>
2017-09-14 15:38:50 -0400

    fixed tests

--- server/tests/test.js
+++ server/tests/test.js
@@ -11,9 +11,10 @@ describe('server', () => {
       });
   });
 
-  it('responds to root request with 200', done => {
+  it('POST to /api responds with json', done => {
     request(app)
       .post('/api')
+      .set('Content-Type', 'application/json')
       .send({
         firstName: 'test name',
         lastName: 'test name',
@@ -22,11 +23,13 @@ describe('server', () => {
       })
       .then(response => {
         expect(response.statusCode).toBe(200);
-        expect(response.contentType).toBe('Application/JSON');
-        expect(response.firstName).toBe('test name');
-        expect(response.lastName).toBe('test name');
-        expect(response.address).toBe('test address');
-        expect(response.compnay).toBe('test company');
+        expect(response.type).toBe('application/json');
+        expect(response.body.firstName).toBe('test name');
+        expect(response.body.lastName).toBe('test name');
+        expect(response.body.address).toBe('test address');
+        expect(response.body.company).toBe('test company');
+        expect(response.body._id).toBeDefined();
+        expect(response.body._id).toBeTruthy();
         done();
       });
   });
